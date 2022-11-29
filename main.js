e47448a305c2623cdfd6fe26495c974693e05ec9
Lokman <104599813+Loke-60000@users.noreply.github.com>
2022-11-29 11:12:58 +0100

    myes

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 let openCookieModal = document.querySelector(".icon_cookies")
 let openNewsModal = document.querySelector(".icon_newsletter")
+let openLoginModal = document.querySelector(".icon_login")
+
 let exitCookieModal = document.querySelector(".exitCookie")
 let exitNewsModal = document.querySelector(".exitNews")
+let exitLoginModal = document.querySelector(".exitlogin")
 
+
+
+
+/* 
+==================================================
+======- cookies -
+==================================================
+*/
 
 openCookieModal.addEventListener("click", function(event) {
     document.querySelector(".cookieModal").setAttribute("style", "")
@@ -17,6 +28,13 @@ exitCookieModal.addEventListener("click", function(event) {
 })
 
 
+/* 
+==================================================
+======- news -
+==================================================
+*/
+
+
 openNewsModal.addEventListener("click", function(event) {
     document.querySelector(".newsModal").setAttribute("style", "")
     document.querySelector("body").setAttribute("style", `background-color: #656565`)
@@ -24,10 +42,26 @@ openNewsModal.addEventListener("click", function(event) {
 })
 
 exitNewsModal.addEventListener("click", function(event) {
-    document.querySelector(".newsModal").setAttribute(
-        "style", 
-        "visibility: hidden;"
-    )
+    document.querySelector(".newsModal").setAttribute("style", "visibility: hidden;")
+    document.querySelector("body").setAttribute("style", `background-color: white`)
+    document.querySelector("footer").setAttribute("style", "opacity: 1;")
+})
+
+/* 
+==================================================
+======- login -
+==================================================
+*/
+
+
+openLoginModal.addEventListener("click", function(event) {
+    document.querySelector(".loginModal").setAttribute("style", "")
+    document.querySelector("body").setAttribute("style", `background-color: #656565`)
+    document.querySelector("footer").setAttribute("style", "opacity: 0;")
+})
+
+exitLoginModal.addEventListener("click", function(event) {
+    document.querySelector(".loginModal").setAttribute("style", "visibility: hidden;")
     document.querySelector("body").setAttribute("style", `background-color: white`)
     document.querySelector("footer").setAttribute("style", "opacity: 1;")
 })
